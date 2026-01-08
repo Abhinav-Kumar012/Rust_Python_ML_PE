068d0c4b7644620c4fe04189b48130d8acd9083b
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-01-08 23:32:25 +0530

    mnist in rust done - 1

--- rust_ml/mnist_ml/src/main.rs
+++ rust_ml/mnist_ml/src/main.rs
@@ -1,3 +1,22 @@
+mod data;
+mod model;
+mod training;
+
+use crate::{model::ModelConfig, training::TrainingConfig};
+use burn::{
+	backend::{Autodiff, Wgpu},
+	optim::AdamConfig,
+};
+
 fn main() {
-	println!("Hello, world!");
+	type MyBackend = Wgpu<f32, i32>;
+	type MyAutodiffBackend = Autodiff<MyBackend>;
+
+	let device = burn::backend::wgpu::WgpuDevice::default();
+	let artifact_dir = "/tmp/guide";
+	crate::training::train::<MyAutodiffBackend>(
+		artifact_dir,
+		TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+		device.clone(),
+	);
 }
