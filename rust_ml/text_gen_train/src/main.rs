5d56d40d33e00c4019686ee53ffc3e48da6d460a
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-02-14 17:01:41 +0530

    text gen - 1

--- rust_ml/text_gen_train/src/main.rs
+++ rust_ml/text_gen_train/src/main.rs
@@ -1,3 +1,26 @@
+#![recursion_limit = "256"]
+mod data;
+mod model;
+mod training;
+use crate::data::DbPediaDataset;
+use burn::optim::decay::WeightDecayConfig;
+use crate::{training::ExperimentConfig};
+type Elem = f32;
+
+type MyBackend = burn::backend::Autodiff<burn::backend::wgpu::Wgpu<Elem,i32,u32>>;
+
 fn main() {
-	println!("Hello, world!");
+    let config = ExperimentConfig::new(
+        burn::nn::transformer::TransformerEncoderConfig::new(384, 1536, 12, 6)
+            .with_norm_first(true),
+        burn::optim::AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(1.0e-6))),
+    );
+
+    crate::training::train::<MyBackend, DbPediaDataset>(
+        MyBackend::default(),
+        DbPediaDataset::train(),
+        DbPediaDataset::test(),
+        config,
+        "/tmp/text-generation",
+    );
 }
