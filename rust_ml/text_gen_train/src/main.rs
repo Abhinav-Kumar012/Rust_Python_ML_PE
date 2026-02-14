dbbd15cc0fdfa482bc606b4b8c44f7e3f2024ac0
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-02-14 18:17:02 +0530

    formatted the code - 1

--- rust_ml/text_gen_train/src/main.rs
+++ rust_ml/text_gen_train/src/main.rs
@@ -3,24 +3,24 @@ mod data;
 mod model;
 mod training;
 use crate::data::DbPediaDataset;
+use crate::training::ExperimentConfig;
 use burn::optim::decay::WeightDecayConfig;
-use crate::{training::ExperimentConfig};
 type Elem = f32;
 
-type MyBackend = burn::backend::Autodiff<burn::backend::wgpu::Wgpu<Elem,i32,u32>>;
+type MyBackend = burn::backend::Autodiff<burn::backend::wgpu::Wgpu<Elem, i32, u32>>;
 
 fn main() {
-    let config = ExperimentConfig::new(
-        burn::nn::transformer::TransformerEncoderConfig::new(384, 1536, 12, 6)
-            .with_norm_first(true),
-        burn::optim::AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(1.0e-6))),
-    );
+	let config = ExperimentConfig::new(
+		burn::nn::transformer::TransformerEncoderConfig::new(384, 1536, 12, 6)
+			.with_norm_first(true),
+		burn::optim::AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(1.0e-6))),
+	);
 
-    crate::training::train::<MyBackend, DbPediaDataset>(
-        burn::backend::wgpu::WgpuDevice::default(),
-        DbPediaDataset::train(),
-        DbPediaDataset::test(),
-        config,
-        "/tmp/text-generation",
-    );
+	crate::training::train::<MyBackend, DbPediaDataset>(
+		burn::backend::wgpu::WgpuDevice::default(),
+		DbPediaDataset::train(),
+		DbPediaDataset::test(),
+		config,
+		"/tmp/text-generation",
+	);
 }
--- rust_ml/text_gen_train/src/training.rs
+++ rust_ml/text_gen_train/src/training.rs
@@ -1,91 +1,91 @@
 use crate::{
-    data::{Gpt2Tokenizer, TextGenerationBatcher, TextGenerationItem, Tokenizer},
-    model::TextGenerationModelConfig,
+	data::{Gpt2Tokenizer, TextGenerationBatcher, TextGenerationItem, Tokenizer},
+	model::TextGenerationModelConfig,
 };
 use burn::{
-    data::{
-        dataloader::DataLoaderBuilder,
-        dataset::{Dataset, transform::SamplerDataset},
-    },
-    lr_scheduler::noam::NoamLrSchedulerConfig,
-    nn::transformer::TransformerEncoderConfig,
-    optim::AdamConfig,
-    prelude::*,
-    record::{CompactRecorder, DefaultRecorder, Recorder},
-    tensor::backend::AutodiffBackend,
-    train::{
-        Learner, SupervisedTraining,
-        metric::{
-            AccuracyMetric, ClassReduction, CpuMemory, CpuTemperature, CpuUse, CudaMetric,
+	data::{
+		dataloader::DataLoaderBuilder,
+		dataset::{Dataset, transform::SamplerDataset},
+	},
+	lr_scheduler::noam::NoamLrSchedulerConfig,
+	nn::transformer::TransformerEncoderConfig,
+	optim::AdamConfig,
+	prelude::*,
+	record::{CompactRecorder, DefaultRecorder, Recorder},
+	tensor::backend::AutodiffBackend,
+	train::{
+		Learner, SupervisedTraining,
+		metric::{
+			AccuracyMetric, ClassReduction, CpuMemory, CpuTemperature, CpuUse, CudaMetric,
 			FBetaScoreMetric, IterationSpeedMetric, LearningRateMetric, LossMetric,
-			PrecisionMetric, RecallMetric,PerplexityMetric,
-        },
-    },
+			PerplexityMetric, PrecisionMetric, RecallMetric,
+		},
+	},
 };
 use std::sync::Arc;
 
 #[derive(Config, Debug)]
 pub struct ExperimentConfig {
-    transformer: TransformerEncoderConfig,
-    optimizer: AdamConfig,
-    #[config(default = 512)]
-    max_seq_length: usize,
-    #[config(default = 6)]
-    batch_size: usize,
-    #[config(default = 50)]
-    num_epochs: usize,
+	transformer: TransformerEncoderConfig,
+	optimizer: AdamConfig,
+	#[config(default = 512)]
+	max_seq_length: usize,
+	#[config(default = 6)]
+	batch_size: usize,
+	#[config(default = 50)]
+	num_epochs: usize,
 }
 
 pub fn train<B: AutodiffBackend, D: Dataset<TextGenerationItem> + 'static>(
-    device: B::Device,
-    dataset_train: D,
-    dataset_test: D,
-    config: ExperimentConfig,
-    artifact_dir: &str,
+	device: B::Device,
+	dataset_train: D,
+	dataset_test: D,
+	config: ExperimentConfig,
+	artifact_dir: &str,
 ) {
-    let tokenizer = Arc::new(Gpt2Tokenizer::default());
-    let batcher = TextGenerationBatcher::new(tokenizer.clone(), config.max_seq_length);
+	let tokenizer = Arc::new(Gpt2Tokenizer::default());
+	let batcher = TextGenerationBatcher::new(tokenizer.clone(), config.max_seq_length);
 
-    let model = TextGenerationModelConfig::new(
-        config.transformer.clone(),
-        tokenizer.vocab_size(),
-        tokenizer.pad_token(),
-        config.max_seq_length,
-    )
-    .init::<B>(&device);
+	let model = TextGenerationModelConfig::new(
+		config.transformer.clone(),
+		tokenizer.vocab_size(),
+		tokenizer.pad_token(),
+		config.max_seq_length,
+	)
+	.init::<B>(&device);
 
-    let dataloader_train = DataLoaderBuilder::new(batcher.clone())
-        .batch_size(config.batch_size)
-        .num_workers(4)
-        .build(SamplerDataset::new(dataset_train, 10_000));
+	let dataloader_train = DataLoaderBuilder::new(batcher.clone())
+		.batch_size(config.batch_size)
+		.num_workers(4)
+		.build(SamplerDataset::new(dataset_train, 10_000));
 
-    let dataloader_test = DataLoaderBuilder::new(batcher)
-        .batch_size(config.batch_size)
-        .num_workers(4)
-        .build(SamplerDataset::new(dataset_test, 1000));
+	let dataloader_test = DataLoaderBuilder::new(batcher)
+		.batch_size(config.batch_size)
+		.num_workers(4)
+		.build(SamplerDataset::new(dataset_test, 1000));
 
-    let accum = 6; // Effective batch size = 6 * 6 = 32.
-    let optim = config.optimizer.init();
-    let lr_scheduler = NoamLrSchedulerConfig::new(0.01 / accum as f64)
-        .with_warmup_steps(6000)
-        .with_model_size(config.transformer.d_model)
-        .init()
-        .unwrap();
-    let f1metric = FBetaScoreMetric::multiclass(1.0, 1, ClassReduction::Macro);
+	let accum = 6; // Effective batch size = 6 * 6 = 32.
+	let optim = config.optimizer.init();
+	let lr_scheduler = NoamLrSchedulerConfig::new(0.01 / accum as f64)
+		.with_warmup_steps(6000)
+		.with_model_size(config.transformer.d_model)
+		.init()
+		.unwrap();
+	let f1metric = FBetaScoreMetric::multiclass(1.0, 1, ClassReduction::Macro);
 	let precissionmetric = PrecisionMetric::multiclass(1, ClassReduction::Macro);
 	let recallmetric = RecallMetric::multiclass(1, ClassReduction::Macro);
-    let training = SupervisedTraining::new(artifact_dir, dataloader_train, dataloader_test)
-        .metric_train(CudaMetric::new())
-        .metric_valid(CudaMetric::new())
-        .metric_train_numeric(AccuracyMetric::new().with_pad_token(tokenizer.pad_token()))
-        .metric_valid_numeric(AccuracyMetric::new().with_pad_token(tokenizer.pad_token()))
-        .metric_train_numeric(PerplexityMetric::new().with_pad_token(tokenizer.pad_token()))
-        .metric_valid_numeric(PerplexityMetric::new().with_pad_token(tokenizer.pad_token()))
-        .metric_train(LossMetric::new())
-        .metric_valid(LossMetric::new())
-        .metric_train(IterationSpeedMetric::new())
-        .metric_train_numeric(LearningRateMetric::new())
-        .metric_train_numeric(precissionmetric.clone())
+	let training = SupervisedTraining::new(artifact_dir, dataloader_train, dataloader_test)
+		.metric_train(CudaMetric::new())
+		.metric_valid(CudaMetric::new())
+		.metric_train_numeric(AccuracyMetric::new().with_pad_token(tokenizer.pad_token()))
+		.metric_valid_numeric(AccuracyMetric::new().with_pad_token(tokenizer.pad_token()))
+		.metric_train_numeric(PerplexityMetric::new().with_pad_token(tokenizer.pad_token()))
+		.metric_valid_numeric(PerplexityMetric::new().with_pad_token(tokenizer.pad_token()))
+		.metric_train(LossMetric::new())
+		.metric_valid(LossMetric::new())
+		.metric_train(IterationSpeedMetric::new())
+		.metric_train_numeric(LearningRateMetric::new())
+		.metric_train_numeric(precissionmetric.clone())
 		.metric_valid_numeric(precissionmetric.clone())
 		.metric_train_numeric(recallmetric.clone())
 		.metric_valid_numeric(recallmetric.clone())
@@ -101,19 +101,19 @@ pub fn train<B: AutodiffBackend, D: Dataset<TextGenerationItem> + 'static>(
 		.metric_train_numeric(CpuMemory::new())
 		.metric_valid_numeric(CpuMemory::new())
 		.with_file_checkpointer(CompactRecorder::new())
-        .with_file_checkpointer(CompactRecorder::new())
-        .grads_accumulation(accum)
-        .num_epochs(config.num_epochs)
-        .summary();
+		.with_file_checkpointer(CompactRecorder::new())
+		.grads_accumulation(accum)
+		.num_epochs(config.num_epochs)
+		.summary();
 
-    let result = training.launch(Learner::new(model, optim, lr_scheduler));
+	let result = training.launch(Learner::new(model, optim, lr_scheduler));
 
-    config.save(format!("{artifact_dir}/config.json")).unwrap();
+	config.save(format!("{artifact_dir}/config.json")).unwrap();
 
-    DefaultRecorder::new()
-        .record(
-            result.model.into_record(),
-            format!("{artifact_dir}/model").into(),
-        )
-        .unwrap();
+	DefaultRecorder::new()
+		.record(
+			result.model.into_record(),
+			format!("{artifact_dir}/model").into(),
+		)
+		.unwrap();
 }
